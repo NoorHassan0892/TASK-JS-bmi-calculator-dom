7b07e876f547f6f153b6f6198501cb93ea38b441
Noor <noor.hassan0892@gmail.com>
2024-04-29 16:48:30 +0300

    done

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  height = height * 0.01;
+  let BMI = weight / height ** 2;
+
+  alert(BMI);
+  if (BMI < 18.5) {
+    alert("Underweight");
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    alert("Healthy weight");
+  } else if (BMI >= 25.0 && BMI <= 29.9) {
+    alert("Overweight");
+  } else {
+    alert("Above Obesity");
+  }
 }
